f97a395bca0396d22357dea3a1535ba8552b0334
Mayukh <mayukhsaikia@yahoo.com>
2021-02-03 16:23:44 +0530

    Added Salutation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+os.system('cls')
 print('''
 ###### Happy Birthday Wisher ######
 This is a program to wish your friend on his/her Birthday ######
@@ -8,6 +9,7 @@ time.sleep(3)
 os.system('cls')
 f=input("From: ")
 t=input("To: ")
+age=input("Enter the age of the birthday boy/girl: ")
 os.system('cls')
 print(f'''
 Happy birthday, {t}. 
@@ -16,7 +18,21 @@ To my one and only, you deserve the blessings showered on you and many other ben
 Smile today and every other day of your life on earth.
 From the one crazy friend named {f}. Wish you a happy Birthday again
 ''')
-time.sleep(3)
+time.sleep(1)
+print('''
+Woww!!! You have grown ...
+''')
+time.sleep(1)
+i=0
+while(i<len(age)):
+    file=age[i]+".txt"
+    with open(f"{file}") as f:
+        ag=f.read()
+    print(ag+"\n")
+    i+=1
+    time.sleep(1)
+print('''years old.''')
 with open("cake.txt") as f:
     cake=f.read()
+time.sleep(1)
 print(cake)
